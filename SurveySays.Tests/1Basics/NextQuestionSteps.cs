ce88ff6733cb67a98352c998b9f2b21257e9f7cd
Jeff Garoutte <jeff.garoutte+simply.Agile@gmail.com>
2012-10-31 21:34:32 -0500

    Added step files

---
+++ SurveySays.Tests/1Basics/NextQuestionSteps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SurveySays.Tests._1Basics
+{
+    [Binding]
+    public class NextQuestionSteps
+    {
+        
+    }
+}
